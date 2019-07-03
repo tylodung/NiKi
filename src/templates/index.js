a9c8aee9c8e49b3b91103fde1a632c6145ad46a8
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-07-03 21:56:26 +0700

    up

--- src/templates/index.js
+++ src/templates/index.js
@@ -17,7 +17,7 @@ const IndexPage = ({ data, pathContext }) => {
 
       <div className="home-template">
 
-        <Header image='https://i.ytimg.com/vi/VbX4FJTPDSw/maxresdefault.jpg' title="Người Đẹp Việt" tagline="Nét đẹp tự nhiên.." />
+        <Header image='https://i.ytimg.com/vi/6ve4ZfR3gxY/maxresdefault.jpg' title="Người Đẹp Việt" tagline="Nét đẹp tự nhiên.." />
       
         <main id="site-main" className="site-main outer" role="main">
 
