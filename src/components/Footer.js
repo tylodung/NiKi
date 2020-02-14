f17f19ccd03663e899cda1c6c9488575236b77c4
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2020-02-14 10:13:41 +0700

    up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () =>
     <footer className="site-footer outer">
         <div className="site-footer-content inner">
             <section className="copyright">
-                <Link to="/">Jeboleth.com</Link> | Người Đẹp Việt
+                <Link to="/">itirc.info</Link> | Người Đẹp Việt
             </section>
             <nav>
                 <a className="social-logo" href="https://youtu.be/N4WTi9EHLqA" >
